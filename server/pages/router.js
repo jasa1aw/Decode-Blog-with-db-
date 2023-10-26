655e5b8c7061972070f50a05fabc06f854690394
jasa1aw <erakrut05@gmail.com>
2023-10-27 00:42:13 +0600

    modified pagination

--- server/pages/router.js
+++ server/pages/router.js
@@ -9,13 +9,15 @@ router.get('/', async(req, res) => {
     const categories = await Categories.findOne({key: req.query.category})
     if(categories){
         options.category = categories._id
+        res.locals.category = req.query.category
     }
+    
     let page = 0;
     const limit = 4;
     if(req.query.page && req.query.page > 0){
         page = req.query.page
     }
-    const totalBlogs = await Blog.count()
+    const totalBlogs = await Blog.count(options)
     const allCategories = await Categories.find();
     const blogs = await Blog.find(options).limit(limit).skip(page * limit).sort({_id: -1}).populate('category').populate('author')
     res.render('main', {categories: allCategories, blogs, user: req.user ? req.user : {}, pages: Math.ceil(totalBlogs / limit)});
